5468abb8d489dbfdd9dc0b7def8acc3f2e29a5e8
pawantech12 <pawankumavat042@gmail.com>
2025-01-08 21:58:32 +0530

    added applicant result page

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ApplicantResult from "./Pages/applicantResult";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ApplicantResult />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+{
+  /* <div className='absolute w-[905px] h-[905px] -mt-[500px] ml-[1100px] bg-[rgba(151,151,151,0.7)] rounded-full z-10 '>
+
+<Spline className='items-end w-20 h-20 -ml-[30%] mt-[30%]' scene="https://prod.spline.design/9KpvIenVCsNv9ycN/scene.splinecode" />
+</div> */
+}
